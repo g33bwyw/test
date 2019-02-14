348749457c6e8af4c3dd725f398436ec495e3d3c
vagrant <vagrant@homestead>
2019-02-14 16:40:32 +0800

    文件提交

---
+++ src/ShumeiCloud.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the openapi package.
+ *
+ * (c) liugj<liugj@boqii.com> wangaibo<wangaibo@boqii.com> jijz<jijz@boqii.com>
+ * (c) zhangsl<zhangsl@boqii.com> wangyt<wangyt@boqii.com> wangyw<wangyw@boqii.com>
+ * (c) caowl<caowl@boqii.com> curry.zheng<curry.zheng@boqii.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace FengKong\Shumei;
+
+class ShumeiCloud
+{
+    public function index()
+    {
+        return 'hello';
+    }
+}
